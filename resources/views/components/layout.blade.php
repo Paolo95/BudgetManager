f8ca0fd57810b61b423a101c8148e8e7e5a1b58e
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2024-01-04 18:42:54 +0100

    Coded first div of the home page
    Insert google fonts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,19 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
   <link rel="icon" href="images/favicon.ico" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
   <script src="//unpkg.com/alpinejs" defer></script>
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
@@ -17,7 +26,7 @@
           extend: {
             colors: {
               laravel: '#03a1fc',
-            },
+            }
           },
         },
       }
@@ -26,6 +35,7 @@
 </head>
 
 <body>
+
   <nav class="flex justify-between items-center mb-4 mt-4">
     <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
@@ -42,7 +52,7 @@
         <form class="inline" method="POST" action="/logout">
           @csrf
           <button type="submit">
-            <i class="fa-solid fa-door-closed"></i> Logout
+            <i class="fa-solid fa-door-closed hover:text-laravel"></i> Logout
           </button>
         </form>
       </li>
@@ -57,17 +67,17 @@
     </ul>
   </nav>
 
-  <main>
-    {{$slot}}
-  </main>
-
   
-  <footer
-    class="bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-    <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
+  <div class="content">
+    {{$slot}}
+  </div>
+  
+  <footer class="flex items-center font-bold bg-laravel text-white h-24 mt-20 opacity-90 md:justify-center">
+    <p class="ml-2">Copyright &copy; 2024, Tutti i diritti riservati</p>
   </footer>
 
   <x-flash-message />
+
 
 </body>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
-    
+  <div class="home-container container px-10 flex flex-row">
+    <div class="home-container1 my-auto">
+        <h1>Benvenuto in Budget Manager</h1>
+        <h2>La piattaforma che ti consente di gestire le tue finanze in modo semplice!</h2>
+        <div class="home-btn-div">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Scopri le funzionalità
+          </button>
+      </div>
+    </div>
+    <div class="home-container2 my-auto">
+      <img class="rounded" src="/images/homepage-photo.jpg" alt="Immagine della homepage">
+      
+    </div>
+  </div>
 </x-layout>
